1b2c53adb2294987b1c37ccb73a127e06227c443
Prakash Natarajan <prakash.atarajan@factset.com>
2017-05-19 18:41:32 -0400

    Changed style of course completed && implemented course compled with async fetch

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -10,11 +10,8 @@ angular.module('myApp.projects', ['ngRoute'])
 }])
 
 .controller('ProjectsCtrl', ['$scope','$http', function($scope,$http) {
-	var COURSES = ['ud899', 'ud898', 'ud356']
+	var COURSES_COMPLETED = ['ud899', 'ud898', 'ud356']
 	$http.get("https://www.udacity.com/public-api/v0/courses")
-	.then(response => {
-			$scope.courses = response.data.courses.filter(course => COURSES.indexOf(course.key) >= 0); 
-			console.log($scope.courses);
-		})
+	.then(response => $scope.coursesCompleted = response.data.courses.filter(course => COURSES_COMPLETED.indexOf(course.key) >= 0))
 	.catch(err => console.log(`err is ${err}`));
 }]);
